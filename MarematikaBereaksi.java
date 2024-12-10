73400d64ad36ec0259495b9e9598a455f998829c
TamaraHinggu <097tamaraa@gmail.com>
2024-12-10 12:07:05 +0700

    Add files via upload

---
+++ MarematikaBereaksi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Marematika;
+
+/**
+ *
+ * @author USER
+ */
+public class MarematikaBereaksi {
+    public static void main(String[] args) {
+        Matematika mtk = new Matematika();
+        mtk.pertambahan(10, 10);
+        mtk.pengurangan(50, 20);
+        mtk.perkalian(4, 10);
+        mtk.pembagian(100, 10);
+    }
+} 
+
